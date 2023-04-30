afa38f25883404659e0ee3f7b2d154ecb43c8263
fatemehafvaj <fatemehafvaj1996@gmail.com>
2023-04-30 12:25:36 +0430

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# react-weather-test
+ for shecode
---
+++ src/Api.js
@@ -0,0 +1,4 @@
+export default class Api {
+  static url = "https://api.openweathermap.org";
+  static key = "06f890ecc77e50e91e6cc5ecb6c1a936";
+}
